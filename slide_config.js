b5b36b819548402c75b9a34ef31672e25848fe25
Florian Uhlig <fuhlig@stud.hs-bremen.de>
2012-12-02 23:35:20 +0000

    push 2 gh-pages

---
+++ slide_config.js
@@ -0,0 +1,36 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Mobile Web Workflow',
+    subtitle: 'Das Web in deiner Hand',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Florian Uhlig',
+    company: 'Student, Hochschule Bremen',
+    gplus: 'https://plus.google.com/112898190514765164004/',
+    // twitter: '@yourhandle',
+    // www: 'http://www.you.com',
+    // github: 'http://github.com/you'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
